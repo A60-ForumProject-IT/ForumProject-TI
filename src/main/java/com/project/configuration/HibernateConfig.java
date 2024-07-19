4e9706d9e84106224dde1c75feef5de1006c14c4
Todor Raynov <t.raynov@abv.bg>
2024-07-19 13:23:11 +0300

    Renamed folder

--- src/main/java/com/project/configuration/HibernateConfig.java
+++ src/main/java/com/project/configuration/HibernateConfig.java
@@ -1,4 +1,4 @@
-package com.project.config;
+package com.project.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;

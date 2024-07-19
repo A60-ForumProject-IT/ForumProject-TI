b202fe90e72ed6f4004f68dfb828babf53071099
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-07-19 17:31:30 +0300

    Add UserDTO class

---
+++ src/main/java/com/project/models/dtos/UserDTO.java
@@ -0,0 +1,33 @@
+package com.project.models.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    @NotNull(message = "Username can't be empty.")
+    @Size(min = 4, max = 32, message = "Username should be between 4 and 32.")
+    private String username;
+
+    @NotNull(message = "Password can't be empty.")
+    @Size(min = 8)
+    private String password;
+
+    @Email(message = "Email is invalid.")
+    private String email;
+
+    @NotNull(message = "First name name can't be empty.")
+    @Size(min = 4, max = 32, message = "First name should be between 4 and 32.")
+    private String firstName;
+
+    @NotNull(message = "Last name can't be empty.")
+    @Size(min = 4, max = 32, message = "Last name should be between 4 and 32.")
+    private String lastName;
+
+    public UserDTO() {}
+}

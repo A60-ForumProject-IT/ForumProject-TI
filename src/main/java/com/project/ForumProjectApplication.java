5e3e338def87260e8df2f7be456d71ca57085f50
Todor Raynov <t.raynov@abv.bg>
2024-07-19 12:30:11 +0300

    Test push

--- src/main/java/com/project/ForumProjectApplication.java
+++ src/main/java/com/project/ForumProjectApplication.java
@@ -8,6 +8,7 @@ public class ForumProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumProjectApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }

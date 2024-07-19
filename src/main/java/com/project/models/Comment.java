ac7c4b56380e3d938b7533c3d0a33424949cd3c1
Todor Raynov <t.raynov@abv.bg>
2024-07-19 17:13:40 +0300

    Comment class updated.

--- src/main/java/com/project/models/Comment.java
+++ src/main/java/com/project/models/Comment.java
@@ -1,12 +1,18 @@
 package com.project.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "comments")
-public class Comment {
+public class Comment implements Comparable<Comment>{
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -25,5 +31,24 @@ public class Comment {
     @JoinColumn(name = "post_id")
     private Post commentedPost;
 
+    public Comment() {
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id == comment.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(Comment o) {
+        return Integer.compare(this.getId(), o.getId());
+    }
 }

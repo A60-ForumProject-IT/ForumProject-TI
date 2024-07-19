42214043f488f535bc2cf56801759647c3f7a9d7
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-07-19 12:27:32 +0300

    Refactor.

---
+++ src/main/java/com/project/ForumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/config/HibernateConfig.java
@@ -0,0 +1,50 @@
+package com.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+//@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
+public class HibernateConfig {
+    private  final String dbUrl, dbUsername, dbPassword;
+
+    @Autowired
+    public HibernateConfig(Environment env) {
+        dbUrl = env.getProperty("database.url");
+        dbUsername = env.getProperty("database.username");
+        dbPassword = env.getProperty("database.password");
+    }
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.telerikacademy.web.beertag.models");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    private Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+        return hibernateProperties;
+    }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        //   dataSource.setUrl("jdbc:mariadb://localhost:3306/beers");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+}

6fd1b605cc3ffd6acd65734ac1fc1b30731070fe
IHKaragyozov19 <ilian_k_19@abv.bg>
2024-08-07 19:26:16 +0300

    Implementing logout

--- src/main/java/com/project/controllers/mvc/AuthenticationMvcController.java
+++ src/main/java/com/project/controllers/mvc/AuthenticationMvcController.java
@@ -54,4 +54,10 @@ public class AuthenticationMvcController {
             return "LoginView";
         }
     }
+
+    @GetMapping("/logout")
+    public String handleLogout(HttpSession session) {
+        session.removeAttribute("currentUser");
+        return "redirect:/";
+    }
 }
